2b7eb7139d17e38b5fdf675b21ab1d9d597b3f69
YanzhenHuang <huangyanzhen0108@163.com>
2024-02-05 14:37:25 +0800

    :pencil: 修改cta部分的themeColor，並用tailwind寫法

--- components/cta.js
+++ components/cta.js
@@ -7,7 +7,7 @@ const Cta = () => {
 
   return (
     <Container>
-      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
+      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-themeColor bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
             {t("Are you ready to use the App?")}
@@ -21,8 +21,8 @@ const Cta = () => {
             href="https://apps.apple.com/us/app/um-all/id1636670554"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
-            AppStore
+            className="inline-block py-3 mx-auto text-themeColor text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
+            App Store
           </a>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
@@ -30,7 +30,7 @@ const Cta = () => {
             href="https://play.google.com/store/apps/details?id=one.umall"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
+            className="inline-block py-3 mx-auto text-themeColor text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
             Android
           </a>
         </div>
@@ -39,7 +39,7 @@ const Cta = () => {
             href="https://umall.one/static/release/app-release.apk"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
+            className="inline-block py-3 mx-auto text-themeColor text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-8 lg:py-3 ">
             HUAWEI
           </a>
         </div>
