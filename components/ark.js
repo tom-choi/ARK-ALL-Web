4fc279fef3aa6512b33ea29cda58df7037b1ccb4
YanzhenHuang <huangyanzhen0108@163.com>
2024-02-05 14:14:30 +0800

    :pencil: 修改介紹部分為tailwind寫法

--- components/ark.js
+++ components/ark.js
@@ -29,24 +29,22 @@ const Ark = () => {
                   href="https://apps.apple.com/us/app/um-all/id1636670554"
                   target="_blank"
                   rel="noopener"
-                  className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
-                  style={{ backgroundColor: COLOR_DIY.themeColor}}>
-                  App Store
+                  className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor"
+                  >
+                  iOS
                 </a>
                 <a
                   href="https://play.google.com/store/apps/details?id=one.umall"
                   target="_blank"
                   rel="noopener"
-                  className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
-                  style={{ backgroundColor: COLOR_DIY.themeColor}}>
+                  className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor">
                   Android
                 </a>
                 <a
                   href="https://umall.one/static/release/app-release.apk"
                   target="_blank"
                   rel="noopener"
-                  className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
-                  style={{ backgroundColor: COLOR_DIY.themeColor}}>
+                  className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor">
                   HUAWEI
                 </a>
                 
