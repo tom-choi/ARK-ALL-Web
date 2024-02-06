f1e581b9c901f58c79b017cc0a8e40e7e13c3fac
YanzhenHuang <huangyanzhen0108@163.com>
2024-02-06 23:02:42 +0800

    :bug: Footer容易引起渲染問題，暫時去除。

--- components/footer.js
+++ components/footer.js
@@ -54,7 +54,7 @@ export default function Footer() {
             </div>
 
             <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
-              {t("footersign")}
+            {/*t("footersign")*/}
             </div>
           </div>
 
