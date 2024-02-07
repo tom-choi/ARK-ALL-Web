60bace8d9f2c4244352db90d0b1ff9a156eec723
YanzhenHuang <huangyanzhen0108@163.com>
2024-02-08 01:05:54 +0800

    :pencil: 社團登錄頁修改debug log

--- pages/clublogin.js
+++ pages/clublogin.js
@@ -1,5 +1,6 @@
 // 包引用
 import React, {useState} from 'react';
+import { Router, Route, Link } from 'react-router';
 import axios from 'axios';
 import qs from 'qs';
 import ReactDOM from "react-dom/client"
@@ -28,7 +29,8 @@ const ClubLogin = () => {
     const handleLoginPress = () => {
         // 賬戶輸入未完成
         if (account == '' || password == '') {
-            console.log("賬密輸入未完成！")
+            console.log("賬密輸入未完成！");
+            window.alert("錯誤！\n賬密輸入未完成！");
         } else {
             clubSignIn();
         }
@@ -52,13 +54,16 @@ const ClubLogin = () => {
                 console.log("登入成功！")
                 localStorage.setItem("isClub",true);
                 localStorage.setItem("ClubData",JSON.stringify(json));
+                window.location.href="./club/clubInfo";
             }
             // 登錄失敗
             else {
-                console.log("登入失敗！")
+                console.log("登入失敗！");
+                window.alert("登入失敗！請檢查賬號密碼是否正確。");
             }
         }).catch(err => {
             console.log("網路錯誤！")
+            window.alert("網路錯誤！")
         });
     }
 
@@ -81,6 +86,7 @@ const ClubLogin = () => {
                                 className="border-2 border-themeColor rounded-lg h-10 p-2"
                                 placeholder="社團賬號"
                                 value={account}
+                                
                                 onChangeCapture={(event) => setAccount(event.target.value)}>
                             </input>
                         </li>
@@ -90,6 +96,7 @@ const ClubLogin = () => {
                                 className="border-2 border-themeColor rounded-lg h-10 p-2"
                                 placeholder="密碼"
                                 value={password}
+                                type="password"
                                 onChangeCapture={(event) => setPassword(event.target.value)}>
                             </input>
                         </li>
