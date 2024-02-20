d1830f719159c69dc14e54a94367c0670077d081
Rookie, LIN <1049825685@qq.com>
2024-02-20 11:17:58 +0800

    :art: 優化Tutorial頁代碼結構
    圖片順序為，先登錄

--- pages/tutorial.js
+++ pages/tutorial.js
@@ -13,24 +13,14 @@ import Imglog from '../public/img/tur/登陸.JPG';
 
 const tutorial = () => {
 
-  const Img1 = {
-    image: Img2_1,
-  };
-  const Img2 = {
-    image: Img2_2,
-  };
-  const Img3 = {
-    image: Img2_3,
-  };
-  const Img4 = {
-    image: Img3_1,
-  };
-  const Img5 = {
-    image: Img3_2,
-  };
-  const Img6 = {
-    image: Imglog,
-  };
+  const imgArr = [
+    Imglog,
+    Img2_1,
+    Img2_2,
+    Img2_3,
+    Img3_1,
+    Img3_2,
+  ];
 
   return (
     <>
@@ -38,61 +28,18 @@ const tutorial = () => {
       <Container className="flex flex-wrap lg:flex-nowrap w-full h-full">
         <div className="flex items-center justify-center w-full h-full lg:w-1/2 grid-cols-2">
           <div className="grid grid-cols-2 gap-4">
-            <Image
-              src={Img1.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img1.image.src}
-              style={{ maxWidth: "100%" }}
-            />
-            <Image
-              src={Img2.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img2.image.src}
-            />
-            <Image
-              src={Img3.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img3.image.src}
-            />
-            <Image
-              src={Img4.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img4.image.src}
-            />
-            <Image
-              src={Img5.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img5.image.src}
-            />
-            <Image
-              src={Img6.image}
-              width="521"
-              height="auto"
-              alt="Benefits"
-              className="object-cover"
-              placeholder="blur"
-              blurDataURL={Img6.image.src}
-            />
+            {imgArr.map(itm => {
+              return <Image
+                src={itm}
+                width="521"
+                height="auto"
+                alt="Benefits"
+                className="object-cover"
+                placeholder="blur"
+                blurDataURL={itm.src}
+                style={{ maxWidth: "100%" }}
+              />
+            })}
           </div>
         </div>
       </Container>
