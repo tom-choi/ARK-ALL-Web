2f0811623b7b20b80c4fdf18a68029bb871f3c31
Rookie, LIN <1049825685@qq.com>
2024-02-20 18:13:23 +0800

    :art: 頂部語言設置按鈕改為一次點擊立即切換

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -11,42 +11,40 @@ const LanguageSwitcher = () => {
 
     const handleLanguageChange = (e) => {
         const selectedLanguage = e.target.value;
-        setLanguage(e.target.value);
+        setLanguage(selectedLanguage);
         i18n.changeLanguage(selectedLanguage);
     };
 
     return (
         <div>
-        <select onChange={handleLanguageChange}>
-            <option value="zh">中文</option>
-            <option value="en">English</option>
-            <option value="ja">日本語</option>
-        </select>
+            <button onClick={handleLanguageChange} value={"zh"} style={{ marginRight: "50px" }}>中</button>
+            <button onClick={handleLanguageChange} value={"en"} style={{ marginRight: "50px" }}>EN</button>
+            <button onClick={handleLanguageChange} value={"ja"} >日</button>
 
-        {i18n.language === "zh" && (
-            <div>
-                <h1>歡迎使用中英文轉換功能！</h1>
-                <p>這是一段中文文本。</p>
-            </div>
-        )}
+            {i18n.language === "zh" && (
+                <div>
+                    <h1>歡迎使用中英文轉換功能！</h1>
+                    <p>這是一段中文文本。</p>
+                </div>
+            )}
 
-        { i18n.language === "en" && (
-            <div>
-                <h1>Welcome to the Language Switcher!</h1>
-                <p>This is an English text.</p>
-            </div>
-        )}
+            {i18n.language === "en" && (
+                <div>
+                    <h1>Welcome to the Language Switcher!</h1>
+                    <p>This is an English text.</p>
+                </div>
+            )}
 
-        { i18n.language === "ja" && (
-            <div>
-                <h1>言語切り替えへようこそ！</h1>
-                <p>これは日本語のテキストです。</p>
-            </div>
-        )}
+            {i18n.language === "ja" && (
+                <div>
+                    <h1>言語切り替えへようこそ！</h1>
+                    <p>これは日本語のテキストです。</p>
+                </div>
+            )}
         </div>
     );
 
-    
+
 };
 
 export default LanguageSwitcher;
