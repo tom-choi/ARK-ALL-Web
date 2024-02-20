56df3d9368c1d0a3290f93e7cc72932ce2384407
Rookie, LIN <1049825685@qq.com>
2024-02-20 11:43:04 +0800

    :art: 主頁增加“下載地址”說明

--- components/ark.js
+++ components/ark.js
@@ -12,80 +12,86 @@ const Ark = () => {
   const { t } = useTranslation();
 
   return (
-      <>
-        <Container className="flex flex-wrap w-full h-full">
-          {/* ARK介紹+下載按鈕 */}
-          <div className="flex items-center w-full lg:w-1/2">
-            <div className="max-w-2xl mb-8">
-              <div className="flex gap-4 items-center">
-                <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-                  ARK ALL
-                </h1>
-              </div>
-              <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-                {t("arkText")}
-              </p>
+    <>
+      <Container className="flex flex-wrap w-full h-full">
+        {/* ARK介紹+下載按鈕 */}
+        <div className="flex items-center w-full lg:w-1/2">
+          <div className="max-w-2xl mb-8">
+            <div className="flex gap-4 items-center">
+              <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+                ARK ALL
+              </h1>
+            </div>
+            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+              {t("arkText")}
+            </p>
 
-              <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-3 xl:mr-40 lg:mr-0 ">
-                <a
-                  href="https://apps.apple.com/us/app/um-all/id1636670554"
-                  target="_blank"
-                  rel="noopener"
-                  className="px-5 py-4 text-lg font-medium text-center text-white rounded-md bg-themeColor hover:bg-themeColorLight"
-                  >
-                  App Store
-                </a>
-                <a
-                  href="https://play.google.com/store/apps/details?id=one.umall"
-                  target="_blank"
-                  rel="noopener"
-                  className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor hover:bg-themeColorLight">
-                  Play Store
-                </a>
-                <a
-                  href="https://umall.one/static/release/app-release.apk"
-                  target="_blank"
-                  rel="noopener"
-                  className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor hover:bg-themeColorLight">
-                  HUAWEI
-                </a>
-                </div>
-                <div className="flex mt-5">
-                <a
+            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+              {t("Download")}
+            </p>
+            <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-3 xl:mr-40 lg:mr-0 ">
+              <a
+                href="https://apps.apple.com/us/app/um-all/id1636670554"
+                target="_blank"
+                rel="noopener"
+                className="px-5 py-4 text-lg font-medium text-center text-white rounded-md bg-themeColor hover:bg-themeColorLight"
+              >
+                App Store
+              </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=one.umall"
+                target="_blank"
+                rel="noopener"
+                className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor hover:bg-themeColorLight">
+                Play Store
+              </a>
+              <a
+                href="https://umall.one/static/release/app-release.apk"
+                target="_blank"
+                rel="noopener"
+                className="px-5 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md bg-themeColor hover:bg-themeColorLight">
+                HUAWEI
+              </a>
+            </div>
+            <div className="flex mt-5">
+              <a
                 href="https://github.com/UM-ARK"
                 target="_blank"
                 rel="noopener"
                 className="flex items-center space-x-2 mr-auto text-gray-500 dark:text-gray-400 hover:opacity-50">
-                  <svg
-                    role="img"
-                    width="24"
-                    height="24"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <title>GitHub</title>
-                    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                  </svg>
-                  <span>View Github Repository</span>
-                </a>
-              </div>
+                <svg
+                  role="img"
+                  width="24"
+                  height="24"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title>GitHub</title>
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                </svg>
+                <span>View Github Repository</span>
+              </a>
             </div>
           </div>
-          {/* 兩個炫酷旋轉的正方體*/}
-          <div className="flex items-center justify-center w-full lg:w-1/2">
-            <Canvas shadows flat linear>
-              <Cube1 />
-            </Canvas>
-            <Canvas shadows flat linear>
-              <Cube2 />
-            </Canvas>
-          </div>
-        </Container>
+        </div>
+
+        {/* 兩個炫酷旋轉的正方體*/}
+        <div className="flex items-center justify-center w-full lg:w-1/2">
+          <Canvas shadows flat linear>
+            <Cube1 />
+          </Canvas>
+          <Canvas shadows flat linear>
+            <Cube2 />
+          </Canvas>
+        </div>
+      </Container>
+
+      {false &&
         <Container>
           <div className="flex flex-col justify-center">
             <div className="text-xl text-center text-gray-700 dark:text-white">
-              {t("tb")} 
+              {t("tb")}
               <span className="text-indigo-600 text-themeColor">2000+</span>{" "}
               {t("siuom")}
             </div>
@@ -115,7 +121,8 @@ const Ark = () => {
             </div>
           </div>
         </Container>
-      </>
+      }
+    </>
   );
 }
 
