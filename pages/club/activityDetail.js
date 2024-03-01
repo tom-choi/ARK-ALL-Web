ab4beb9ce8585239f2b924837d71185817f13c50
Rookie, LIN <1049825685@qq.com>
2024-03-01 17:50:05 +0800

    :art: 優化活動詳情頁代碼結構

--- pages/club/activityDetail.js
+++ pages/club/activityDetail.js
@@ -58,6 +58,7 @@ const ActivityDetail = () => {
     // 相關圖片
     const [m_relatedImages, setAddedRelatedImages] = useState(null);     // 暫存活動圖片
 
+    const [isLoading, setIsLoading] = useState(true);
 
     // 從本地緩存中獲取活動資料
     const fetchActivityData = () => {
@@ -79,6 +80,7 @@ const ActivityDetail = () => {
 
             // 簡介
             curActivityInfo.m_intro && setIntro(curActivityInfo.m_intro);
+            setIsLoading(false);
         }
     }
 
@@ -108,7 +110,7 @@ const ActivityDetail = () => {
 
     // 檢測是否被編輯過
     const isEdited = () => {
-
+        return false
     }
 
     // 刪除活動
@@ -182,46 +184,38 @@ const ActivityDetail = () => {
     /*---------------------------------初始化----------------------------------*/
     useEffect(() => {
         fetchActivityData();
-    },
-        []
-    );
+    }, []);
 
     const coverImageRef = useRef();
     const relateImageInputRef = useRef();
 
+    return (<>
+        <title>
+            {activityData && activityData.title} - 詳情
+        </title>
 
-    return (
-
-
-        <>
-            <title>
-                {activityData && activityData.title} - 詳情
-            </title>
-
-            <Container>
-                {/* 頂欄*/}
-                <div className="flex justify-between items-center mb-10 mb-5">
-                    <div className="flex items-center  text-themeColor text-xl font-bold">
-                        <div className="flex flex-col justify-center">
-                            <ChevronLeftIcon className="w-5 h-5" />
-                        </div>
-                        <div
-                            className=" hover:cursor-pointer hover:opacity-50"
-                            onClick={returnToClubInfo}>
-                            返回{activityData && activityData.club_name}
-                        </div>
+        <Container>
+            {/* 頂欄*/}
+            <div className="flex justify-between items-center mb-5">
+                <div className="flex items-center  text-themeColor text-xl font-bold">
+                    <div className="flex flex-col justify-center">
+                        <ChevronLeftIcon className="w-5 h-5" />
                     </div>
-                    <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-                        <ThemeChanger />
-                        <LanguageSwitcher />
+                    <div
+                        className=" hover:cursor-pointer hover:opacity-50"
+                        onClick={returnToClubInfo}>
+                        返回{activityData && activityData.club_name}
                     </div>
                 </div>
+                <div className="hidden mr-3 space-x-4 lg:flex nav__item">
+                    <ThemeChanger />
+                    <LanguageSwitcher />
+                </div>
+            </div>
 
+            {!isLoading && (<>
                 {/* 社團名字+活動標題*/}
                 <div className="flex flex-col items-center text-themeColor font-bold mb-5">
-                    <h3 className="text-xl mb-3">
-                        {activityData && activityData.club_name}
-                    </h3>
                     {!isEditMode ? (
                         <h1 className="text-3xl">
                             {activityData && activityData.title}
@@ -234,6 +228,10 @@ const ActivityDetail = () => {
                             onChangeCapture={(event) => setTitle(event.target.value)}>
                         </input>
                     )}
+                    {/* 社團名字 */}
+                    <h3 className="text-xl mb-3">
+                        {'By ' + (activityData && activityData.club_name)}
+                    </h3>
                 </div>
 
                 {/* 封面圖片 */}
@@ -275,15 +273,11 @@ const ActivityDetail = () => {
                 {/* 編輯提醒*/}
                 {isEdited() ? (
                     <div class="font-bold text-warning">
-                        <p>
-                            您有編輯待上傳，刷新就會失效！
-                        </p>
+                        <p>您有編輯待上傳，刷新就會失效！</p>
                     </div>
                 ) : (
                     <div class="font-bold text-success">
-                        <p>
-                            所有編輯均已上傳！
-                        </p>
+                        <p>所有編輯均已上傳！</p>
                     </div>
                 )}
 
@@ -298,15 +292,15 @@ const ActivityDetail = () => {
                         {/* 開始時間和結束時間*/}
                         <p>
                             <span className="text-themeColor font-bold">
-                                Start:{'  '}
+                                開始:{'  '}
                             </span>
-                            {activityData && activityData.timestamp}
+                            {activityData && moment(activityData.timestamp).format("YYYY-MM-DD HH:mm")}
                         </p>
                         <p>
                             <span className="text-themeColor font-bold">
-                                End:{'  '}
+                                結束:{'  '}
                             </span>
-                            {activityData && activityData.enddatetime}
+                            {activityData && moment(activityData.enddatetime).format("YYYY-MM-DD HH:mm")}
                         </p>
                         {/* 地點 */}
                         <p>
@@ -361,56 +355,55 @@ const ActivityDetail = () => {
                 </div>
 
                 {/* 相關圖片 (如果沒有相關圖片就不展示該模塊) */}
-                {activityData && (activityData.relate_image_url.length > 0 || isEditMode) &&
-                    (
-                        <div className="bg-white dark:bg-gray-800 border-l-4 border-themeColorLight px-5 pt-3 pb-5 rounded-lg drop-shadow-md itmes-center mb-5">
-                            <div className="mb-3">
-                                <h3 className="text-xl font-bold text-themeColor">相關圖片</h3>
-                            </div>
-                            <div className="lg:grid lg:grid-cols-4 md:block lg:gap-4 items-top justify-center mt-5">
-                                {/* 一般的相關圖片 */}
-                                {activityData && activityData.relate_image_url.map((item, index) => (
-                                    <div key={index} className="flex flex-col mb-4 hover:cursor-pointer hover:opacity-80">
-                                        <a href={BASE_HOST + item} target="_blank">
-                                            <img src={BASE_HOST + item} className="rounded-lg" style={{ backgroundColor: '#fff' }} />
-                                        </a>
-                                    </div>
-                                ))}
-                                {/* 添加圖片模塊：僅在編輯圖片時展示 */}
-                                {
-                                    isEditMode && (
-                                        <div className="flex flex-col items-center justify-center bg-themeColorUltraLight dark:bg-gray-700 rounded-lg border-4 border-themeColor border-dashed min-h-24 hover:cursor-pointer hover:opacity-50 mb-4"
-                                            onClick={event => relateImageInputRef.current.click()}>
-                                            <PlusCircleIcon className="w-10 h-10 text-themeColor" />
-                                            <input
-                                                type="file"
-                                                accept="image/*"
-                                                multiple
-                                                ref={relateImageInputRef}
-                                                onChange={event => handleFileChange(event, "relate")}
-                                                className="flex w-full h-full hidden"
-                                            />
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            {/* 添加的圖片名稱，編輯時展示 */}
-                            <div>
-                                {
-                                    isEditMode && (
-                                        <div className="flex items-center">
-                                            <p className="font-bold text-lg">
-                                                添加的圖片：
-                                            </p>
-                                            <span>
-                                                {m_relatedImages && m_relatedImages.name}
-                                            </span>
-                                        </div>
-                                    )
-                                }
-                            </div>
+                {activityData && (activityData.relate_image_url.length > 0 || isEditMode) && (
+                    <div className="bg-white dark:bg-gray-800 border-l-4 border-themeColorLight px-5 pt-3 pb-5 rounded-lg drop-shadow-md itmes-center mb-5">
+                        <div className="mb-3">
+                            <h3 className="text-xl font-bold text-themeColor">相關圖片</h3>
                         </div>
-                    )}
+                        <div className="lg:grid lg:grid-cols-4 md:block lg:gap-4 items-top justify-center mt-5">
+                            {/* 一般的相關圖片 */}
+                            {activityData && activityData.relate_image_url.map((item, index) => (
+                                <div key={index} className="flex flex-col mb-4 hover:cursor-pointer hover:opacity-80">
+                                    <a href={BASE_HOST + item} target="_blank">
+                                        <img src={BASE_HOST + item} className="rounded-lg" style={{ backgroundColor: '#fff' }} />
+                                    </a>
+                                </div>
+                            ))}
+                            {/* 添加圖片模塊：僅在編輯圖片時展示 */}
+                            {
+                                isEditMode && (
+                                    <div className="flex flex-col items-center justify-center bg-themeColorUltraLight dark:bg-gray-700 rounded-lg border-4 border-themeColor border-dashed min-h-24 hover:cursor-pointer hover:opacity-50 mb-4"
+                                        onClick={event => relateImageInputRef.current.click()}>
+                                        <PlusCircleIcon className="w-10 h-10 text-themeColor" />
+                                        <input
+                                            type="file"
+                                            accept="image/*"
+                                            multiple
+                                            ref={relateImageInputRef}
+                                            onChange={event => handleFileChange(event, "relate")}
+                                            className="flex w-full h-full hidden"
+                                        />
+                                    </div>
+                                )
+                            }
+                        </div>
+                        {/* 添加的圖片名稱，編輯時展示 */}
+                        <div>
+                            {
+                                isEditMode && (
+                                    <div className="flex items-center">
+                                        <p className="font-bold text-lg">
+                                            添加的圖片：
+                                        </p>
+                                        <span>
+                                            {m_relatedImages && m_relatedImages.name}
+                                        </span>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                )}
 
                 {/*操作陣列*/}
                 {isEditMode && (
@@ -439,10 +432,12 @@ const ActivityDetail = () => {
                         </div>
                     </div>
                 )}
-            </Container>
-            <Footer />
-        </>
-    );
+            </>)}
+
+        </Container>
+
+        <Footer />
+    </>);
 }
 
 export default ActivityDetail;
