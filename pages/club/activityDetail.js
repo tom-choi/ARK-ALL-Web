9cdb52e8474f228e608706265e322bfa40cc2325
Rookie, LIN <1049825685@qq.com>
2024-03-14 20:41:20 +0800

    :art: 整理活動編輯頁封面圖片選擇的代碼

--- pages/club/activityDetail.js
+++ pages/club/activityDetail.js
@@ -2,8 +2,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Router, Route, Link } from 'react-router';
 import axios from 'axios';
-import qs from 'qs';
-import ReactDOM from "react-dom/client"
 import {
     PencilSquareIcon,
     TrashIcon,
@@ -21,8 +19,6 @@ import Navbar from '../../components/navbar';
 import ThemeChanger from '../../components/DarkSwitch';
 import LanguageSwitcher from '../../components/LanguageSwitcher';
 import Footer from "../../components/footer";
-import { act } from 'react-three-fiber';
-import { Form } from 'react-hook-form';
 
 
 // 活動類型映射
@@ -39,7 +35,7 @@ const ActivityDetail = () => {
 
     /* -------------------------------編輯狀態--------------------------------*/
     // 標題和封面圖
-    const [m_title, setTitle] = useState(null);                 //社團名稱
+    const [m_title, setTitle] = useState("");
     const [m_coverImage, setCoverImage] = useState(null);
 
     // 基本訊息
@@ -48,15 +44,16 @@ const ActivityDetail = () => {
     const [m_eDate, setEndDate] = useState(moment(new Date()).format("YYYY-MM-DD"));     // 結束日期
     const [m_eTime, setEndTime] = useState(moment(new Date()).format("HH:MM"));          // 結束時間
 
-    const [m_location, setLocation] = useState(null);                           // 地點
-    const [m_link, setLink] = useState(null);
+    const [m_location, setLocation] = useState("");                           // 地點
+    const [m_link, setLink] = useState("");
     const [m_type, setType] = useState("ACTIVITY");
 
     // 簡介
     const [m_intro, setIntro] = useState(null);                       // 描述
 
     // 相關圖片
-    const [m_relatedImages, setAddedRelatedImages] = useState(null);     // 暫存活動圖片
+    const [m_relatedImages, setRelatedImages] = useState(null);     // 暫存活動圖片
+    let del_relate_image = [];
 
     const [isLoading, setIsLoading] = useState(true);
 
@@ -75,7 +72,7 @@ const ActivityDetail = () => {
         if (curActivityInfo) {
             // 封面和標題
             curActivityInfo.m_title && setTitle(curActivityInfo.m_title);
-            //createdActivityInfo.m_coverImage && setCoverImage(createdActivityInfo.m_coverImage);
+            curActivityInfo.cover_image_url && setCoverImage(curActivityInfo.cover_image_url);
 
             // 基本訊息
             curActivityInfo.m_sDate && setStartDate(curActivityInfo.m_sDate);
@@ -108,12 +105,73 @@ const ActivityDetail = () => {
     }
 
     // 上傳編輯
-    const uploadEdit = () => {
-        // TODO: 調用API上傳， commit & push
+    const uploadEdit = async () => {
+        let data = new FormData();
+        data.append('id', activityData._id);
+        data.append('title', m_title);
+        data.append('type', m_type);
+        data.append('link', m_link);
+        if (m_coverImage != activityData.cover_image_url) {
+            // 此時m_coverImage是file類型
+            data.append('cover_image_file', m_coverImage);
+        }
+        // TODO: relate images
+        return
+        // 上傳圖片，form data類型上傳數組需要逐個加入
+        if (add_relate_image.length > 0) {
+            add_relate_image.map(item => {
+                data.append('add_relate_image', {
+                    name: item.fileName,
+                    type: item.type,
+                    uri:
+                        Platform.OS === 'android'
+                            ? item.uri
+                            : item.uri.replace('file://', ''),
+                });
+            });
+        } else {
+            data.append('add_relate_image', '[]');
+        }
+        if (mode == 'edit') {
+            if (del_relate_image.length > 0) {
+                // 刪除後綴，根據21.07.30的後端標準，圖片需後端相對路徑
+                let delHostArr = [];
+                del_relate_image.map(itm => {
+                    delHostArr.push(itm.slice(BASE_HOST.length));
+                });
+                del_relate_image = delHostArr;
+                data.append(
+                    'del_relate_image',
+                    JSON.stringify(del_relate_image),
+                );
+            } else {
+                data.append('del_relate_image', '[]');
+            }
+        }
 
-        // 如果上傳成功了就寫入localStorage，刷新頁面。
+        // TODO: 圖片壓縮
 
-        setEditMode(false);
+        let URL = BASE_URI + POST.EVENT_EDIT;
+        return;
+        await axios.post(URL, data, {
+            withCredentials: true,
+        }).then(res => {
+            let json = res.data;
+            // 上傳成功
+            if (json.message == 'success') {
+                alert('上傳成功');
+                // TODO: 如果上傳成功了就寫入localStorage，刷新頁面。
+                window.location.href = "./activityDetail";
+                setEditMode(false);
+            }
+            else {
+                alert('上傳失敗');
+                console.log(json);
+            }
+        }).catch(err => {
+            console.log('err', err);
+            alert('請求錯誤');
+        });
     }
 
     // 檢測是否被編輯過
@@ -153,7 +211,6 @@ const ActivityDetail = () => {
     function handleFileChange(event, type) {
         if (type === "cover") {
             // 封面圖片
-            //let image = URL.createObjectURL(event.target.files[0]);
             let imgFileObj = event.target.files[0];
             setCoverImage(imgFileObj);
         } else if (type === "relate") {
@@ -176,8 +233,14 @@ const ActivityDetail = () => {
                 return;
             }
 
-            setAddedRelatedImages(imgArr);
+            setRelatedImages(imgArr);
         }
+    }
+
+    // TODO: 圖片刪除
+    const handleImageDelete = (index) => {
+        // 如果選擇刪除的圖片處於服務器數據中，上傳需刪除圖片的資訊
+
     }
 
     /*---------------------------------頁間導航--------------------------------*/
@@ -233,17 +296,43 @@ const ActivityDetail = () => {
 
                 {/* 封面圖片 */}
                 <div className="flex flex-col items-center mb-5">
-                    <img
-                        className="w-96 shadow-lg rounded-xl"
-                        style={{ backgroundColor: '#fff' }}
-                        src={activityData && BASE_HOST + activityData.cover_image_url} />
+                    <img className="w-96 shadow-lg rounded-xl" style={{ backgroundColor: '#fff' }}
+                        src={typeof m_coverImage == 'object' ? URL.createObjectURL(m_coverImage) : BASE_HOST + m_coverImage}
+                    />
+                    {/* 更換圖片按鈕 */}
+                    {isEditMode && (<div className="absolute flex items-center justify-center mt-3">
+                        <div className="flex flex-col bg-black text-white
+                            text-xl p-3 rounded-lg text-center justify-center opacity-50 
+                            hover:cursor-pointer hover:opacity-100"
+                            onClick={() => coverImageRef.current.click()}
+                        >
+                            <p>更換</p>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                ref={coverImageRef}
+                                onChange={event => handleFileChange(event, "cover")}
+                                className="w-full h-full hidden"
+                            />
+                        </div>
+                        {activityData.cover_image_url != m_coverImage && (
+                            <div className="ml-5 flex flex-col bg-black text-white
+                            text-xl p-3 rounded-lg text-center justify-center opacity-50 
+                            hover:cursor-pointer hover:opacity-100"
+                                onClick={() => setCoverImage(activityData.cover_image_url)}
+                            >
+                                <p>還原</p>
+                            </div>
+                        )}
+                    </div>)}
                 </div>
 
                 {/*操作陣列*/}
                 <div className="flex items-center justify-center my-10">
                     {/* 編輯按鈕*/}
-                    <div className="flex items-center justify-center mx-5" onClick={isEditMode ? discardEdit : startEdit}>
-                        <div className="flex bg-themeColor py-3 px-5 rounded-full text-white hover:opacity-50 hover:cursor-pointer">
+                    <div className="flex items-center justify-center mr-5" onClick={isEditMode ? discardEdit : startEdit}>
+                        <div className="flex bg-themeColor py-3 px-5 rounded-full text-white 
+                        hover:opacity-50 hover:cursor-pointer">
                             <div className="flex flex-col justify-center">
                                 <PencilSquareIcon className="w-5 h-5" />
                             </div>
@@ -252,15 +341,16 @@ const ActivityDetail = () => {
                             </div>
                         </div>
                     </div>
-                    {/* TODO: 刪除按鈕*/}
+                    {/* TODO: 刪除活動按鈕*/}
                     {isEditMode && (
-                        <div className="flex items-center justify-center mx-5" onClick={deleteActivity}>
-                            <div className="grid grid-cols-2  bg-alert py-3 px-5 rounded-full text-white hover:opacity-50 hover:cursor-pointer">
+                        <div className="flex items-center justify-center ml-5" onClick={deleteActivity}>
+                            <div className="flex bg-alert py-3 px-5 rounded-full text-white 
+                            hover:opacity-50 hover:cursor-pointer">
                                 <div className="flex flex-col justify-center">
                                     <TrashIcon className="w-5 h-5" />
                                 </div>
-                                <div className="flex flex-col justify-center">
-                                    <span>刪除</span>
+                                <div className="flex flex-col justify-center ml-3">
+                                    <span>刪除活動</span>
                                 </div>
                             </div>
                         </div>
@@ -361,7 +451,7 @@ const ActivityDetail = () => {
                         <div className="grid grid-cols-4 gap-4 items-top justify-center mt-5">
                             {m_relatedImages && m_relatedImages.map((item, index) => (
                                 <div key={index} className="flex mb-4 items-center justify-center" >
-                                    <img src={URL.createObjectURL(item)} className="rounded-lg" />
+                                    <img src={URL.createObjectURL(item)} className="rounded-lg border-themeColor border-4" />
                                     {/* 刪除按鈕 */}
                                     <div className="absolute flex flex-col 
                                     bg-black text-white
@@ -372,7 +462,7 @@ const ActivityDetail = () => {
                                                 // 克隆一次File Object，使用JSON會使File Object變為{}
                                                 let arr = m_relatedImages.map(i => new File([i], i.name, { type: i.type }))
                                                 arr.splice(index, 1);
-                                                setAddedRelatedImages(arr);
+                                                setRelatedImages(arr);
                                             }
                                         }}>
                                         <p>刪除</p>
@@ -413,7 +503,7 @@ const ActivityDetail = () => {
                             </div>
                         </div>
                         {/* 上傳*/}
-                        <div className="flex items-center justify-center mx-5" onClick={saveEdit}>
+                        <div className="flex items-center justify-center mx-5" onClick={uploadEdit}>
                             <div className="flex bg-themeColor py-3 px-5 rounded-full text-white hover:opacity-50 hover:cursor-pointer">
                                 <div className="flex flex-col justify-center">
                                     <ArrowUpIcon className="w-5 h-5" />
