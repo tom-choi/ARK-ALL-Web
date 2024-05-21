9977070e71e104a80b194af4655c9f5cc6e2fb14
Yanzhen Huang <huangyanzhen0108@163.com>
2024-05-21 17:18:19 +0800

    :pencil: 為ContentBlock和ContentBlockGrid寫文檔注釋

--- components/uiComponents/ContentBlock.js
+++ components/uiComponents/ContentBlock.js
@@ -1,3 +1,9 @@
+/**
+ * 帶標題的標準内容卡片
+ * @param {*} props 
+ * @prop {string} title 内容卡片的標題
+ * @returns 
+ */
 export const ContentBlock = (props) => {
     return (
         <div className="bg-white dark:bg-gray-800 border-l-4 border-themeColorLight px-5 pt-3 pb-5 rounded-lg drop-shadow-md itmes-center mb-5">
@@ -10,6 +16,11 @@ export const ContentBlock = (props) => {
     );
 }
 
+/**
+ * 内容卡片分欄
+ * @param {*} props 
+ * @returns 
+ */
 export const ContentBlockGrid = (props) => {
     return (
         <div className="lg:grid lg:grid-cols-2 md:block gap-4 items-top justify-center mt-5">
