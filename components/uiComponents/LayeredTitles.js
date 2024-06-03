5f2a6315f0e6d88dc74ce788f3158f6d12db7dfb
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-03 20:25:24 +0800

    :pencil: 優化標題樣式代碼

--- components/uiComponents/LayeredTitles.js
+++ components/uiComponents/LayeredTitles.js
@@ -1,16 +1,29 @@
 
+// 所有標題公共樣式
+let commonStyles = "text-xl font-bold text-themeColor";
+
+/**
+ * 一級標題
+ * @param {*} props 
+ * @returns 
+ */
 export const FirstTitle = (props) => {
     return (
         <div className="mb-3">
-            <h3 className="text-xl font-bold text-themeColor">{props.children}</h3>
+            <h3 className={commonStyles}>{props.children}</h3>
         </div>
     );
 }
 
+/**
+ * 二級標題
+ * @param {*} props 
+ * @returns 
+ */
 export const SecondTitle = (props) => {
     return (
-        <div className="mb-3">
-            <h3 className="text-lg font-bold text-themeColor">{props.children}</h3>
+        <div className="mb-2 mt-3">
+            <h3 className={commonStyles}>{props.children}</h3>
         </div>
     );
 }
