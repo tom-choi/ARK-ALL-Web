7a49e2c2a518f40d923a955ffad692ae38fbb6de
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-20 14:13:46 +0800

    :bee: Inputs工具添加文檔

--- components/uiComponents/Inputs.tsx
+++ components/uiComponents/Inputs.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
 import { IF } from "./ContentBlock";
 
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
@@ -18,23 +18,33 @@ const TextInputStyles = {
 
 
 /**
- * 基於reac-hook-form封裝的ARK標準表單輸入框。
+ * 基於reac-hook-form封裝的ARK標準表單輸入框，在登錄認證時使用。
  * @param {*} props 
  * @prop {placeholder:string, type:string, isRequired:bool} base - 基礎表單屬性
  * @prop {string} regName - 注冊名，用於辨識表單中不同的輸入框。
- * @prop {*} thisErr - 由UseFormState提供的errors項。
- * @prop {function} register - react-hook-form提供的register函數，將regName注冊到表單中。
+ * @prop {FieldErrors<T>} thisErr - useForm中的formstate中的errors項。使用時需要細化至regName。
+ * @prop {string} errText - 儅輸入框内容未被正確填寫時的提示詞。
+ * @prop {UseFormRegister<T>} register - react-hook-form提供的register函數，將regName注冊到表單中。
  * @example
 *   <ARKTextInput
         base={{ placeholder: t("CLUB_PWD"), type: "password", isRequired: true }}
         regName={"password"}
         thisErr={errors.password}
         errText={"請輸入密碼"}
-        register={register}
-    />
+        register={register}/>
  * @returns 
  */
-export const ARKTextInput = (props) => {
+export const ARKTextInput = (props: {
+    base: {
+        placeholder: string,
+        type: string,
+        isRequired: string
+    },
+    regName: string,
+    thisErr: any,
+    errText: string,
+    register: any
+}) => {
     // 表單屬性
     let { placeholder, type, isRequired } = props.base;
     let { regName, thisErr, errText, register } = props;
@@ -77,11 +87,22 @@ export const ARKTextInput = (props) => {
 }
 
 /**
- * 
- * @param {*} props 
+ * ARK帶標簽的表單。
+ * @param {*} props
+ * @prop {string} title - 表單的標簽，如“開始時間”， “結束時間”等
+ * @prop {boolean} condition - 渲染條件。有些表單選項會根據條件顯示或隱藏。
+ * @prop {ReactNode | ReactNode[]} - 子内容，通常為表單元素。
+ * @example
+ * <ARKLabeledInput
+ *      title={"活動鏈接"}                           // 輸入框的標簽
+ *      condition={watch("type") == "WEBSITE"}>     // 僅在表單選項type為WEBSITE的時候顯示
+ *      <input                                      // 在使用該組件的頁面中的子項目，是一個input 元素，以填寫鏈接爲例子。
+ *          type={"text"}
+ *          {...register("link", {required: watch("type") == "WEBSITE" ? ("需要提供活動鏈接哦！"):false})}/>
+ * </ARKLabeledInput>
  * @returns 
  */
-export const ARKLabeledInput = (props) => {
+export const ARKLabeledInput = (props: { title: string, condition?: boolean, children: ReactNode | ReactNode[] }) => {
     const { title, condition } = props;
     return (
         <IF condition={condition || condition == void 0}>
@@ -96,6 +117,24 @@ export const ARKLabeledInput = (props) => {
 }
 
 
+/**
+ * 基於react-hook-form封裝的ARK標準圖片輸入組件。
+ * @param props 
+ * @prop {string}  base.regName - 註冊名，用於辨識表單中不同的輸入框。
+ * @prop {boolean} base.isRequired - 是否為必須。不填默認為否。
+ * @prop {string} base.initialURL - 初始圖片URL，即該組件未添加圖片的初始圖片。
+ * @prop {UseFormRegister<T>} register - useForm的register函數，將輸入框綁定至表單。
+ * @prop {UseFormSetValue<T>} setValue - useForm的setValue函數，手動將表單中的某項設定為某個值。
+ * @example
+ * setValue("cover_image_file", "https://example.com/image.jpg") // 將cover_image_file這一值設定為"https://example.dom/image.jpg"。
+ * @prop {string} errText - 儅輸入框内容未被正確填寫時的提示詞。
+ * @prop {FieldErrors<T>} thisErr - useForm中的formstate中的errors項。使用時需要細化至regName。
+ * @example thisErr的用法
+ * <ARKImageInput
+ *      // 其他參數
+ *      thisErr={errors.cover_image_file}/>
+ * @returns 
+ */
 export const ARKImageInput = (props: {
     base: {
         regName: string,
@@ -161,14 +200,22 @@ export const ARKImageInput = (props: {
 
 
 /**
- * 基於react-hook-form封裝的ARK標準圖片列表輸入框。
+ * 基於react-hook-form封裝的ARK標準圖片**列表**輸入組件。
  * @param {*} props 
- * @prop {{regName: string, isRequired: boolean, numLimit: int}} - 基礎表單屬性。
- * @prop {function} register - react-hook-form提供的register函數，將regName注冊到表單中。
- * @prop {function} setValue - react-hook-form提供的setValue函數，將值寫入表單中。
+ * @prop {string} base.regName - 注冊名，用於辨識表單中不同的輸入框。
+ * @prop {boolean} base.isRequired - 是否爲必須，不填默認爲否。
+ * @prop {number} base.numLimit - 文件數量限制。
+ * @prop {"array" | "object"} base.mode - 輸入模式，可以將項目推入對象(object)，也可將項目推入數組(Array)。
+ * @prop {UseFormRegister<T>} register - useForm提供的register函數，將regName注冊到表單中。
+ * @prop {*} imgList - 圖片列表，可以是對象(object)或數組(Array)，是所在表單中的監聽值，使用watch傳入。
+ * @example
+ * <ARKListImageInput
+ *      base={{ regName: "add_relate_image", isRequired: false, numLimit: 5 }}
+ *      // 其他參數
+ *      imgList={watch("add_relate_image")}/>   // 此處watch為useForm中定義的函數，用於監聽表單的數值。其中"add_relate_image"應當與regName一致。
+ * @prop {UseFormSetValue<T>} setValue - react-hook-form提供的setValue函數，將值寫入表單中。
  * @prop {string} errText - 輸入錯誤時的提示文字。
- * @prop {object} thisErr - 當前表單欄位的錯誤訊息。
- * @prop {array} imgList - 當前表單欄位的值，在表單中通過watch(regName)傳入。
+ * @prop {FieldErrors<T>} thisErr -  儅輸入框内容未被正確填寫時的提示詞。
  * @returns 
  */
 export const ARKListImageInput = (props: {
@@ -277,6 +324,7 @@ export const ARKListImageInput = (props: {
                     <div className="flex items-center justify-center mb-2">
                         <PlusCircleIcon className="w-10 h-10 text-themeColor" />
                     </div>
+
                     {/* 輸入邏輯部分 */}
                     <input
                         type={"file"}
@@ -332,6 +380,3 @@ export const ARKListImageInput = (props: {
     );
 }
 
-
-
-/** */
