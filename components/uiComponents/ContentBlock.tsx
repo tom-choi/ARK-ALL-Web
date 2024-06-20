51c94094aa5fc076e0bd2c805527d24c9c578f0a
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-20 14:43:49 +0800

    :bee: ContentBlock文檔

--- components/uiComponents/ContentBlock.tsx
+++ components/uiComponents/ContentBlock.tsx
@@ -4,7 +4,19 @@ import Container from "../container";
 
 /**
  * ARK 標準渲染頁面。
- * @prop {string} title - 頁面標題，即瀏覽器標籤頁標題
+ * @abstract
+ * 請停止使用空標簽，即`<></>`，而使用此組件。
+ * @prop {string} title - 頁面標題，即瀏覽器標籤頁標題。
+ * @prop {string|undefined} className - 頁面額外樣式。
+ * @prop {ReactNode | ReactNode[]} children - 頁面内容。
+ * @example
+ * export const Page = () => {
+ *     return (
+ *         <ARKMain title="Page Title">     // 定義頁面標題
+ *             <YOUR_CONTENT>               // 所有頁面内容都在這裏
+ *         </ARKMain>
+ *     );
+ * }
  * @returns 
  */
 export const ARKMain = (props: {
@@ -25,10 +37,12 @@ export const ARKMain = (props: {
 }
 
 /**
- * ARK 帶標題的標準内容卡片
+ * ARK 帶標題的標準内容卡片。
  * @prop {boolean} condition - 卡片渲染的條件，為false時卡片不渲染。默認爲true。
  * @prop {string|undefined} title - 内容卡片的標題。
- * @prop {string|undefined} withTitle - 卡片是否顯示標題。
+ * @prop {string|undefined} className - 卡片的額外樣式。
+ * @prop {string|undefined} styles.withTitle - 卡片是否顯示標題。
+ * @prop {string|undefined} styles.withBackground - 卡片是否具有背景色。
  * @returns 
  */
 export const ContentBlock = (props: {
@@ -59,8 +73,8 @@ export const ContentBlock = (props: {
 }
 
 /**
- * ARK 内容卡片分欄
- * @prop {number} gridNum - 欄位數
+ * ARK 内容卡片分欄。
+ * @prop {number|undefined} gridNum - 欄位數，默認爲左右均分。
  * @returns 
  */
 export const ContentBlockGrid = (props: { gridNum?: number, children: ReactNode | ReactNode[] }) => {
@@ -73,7 +87,29 @@ export const ContentBlockGrid = (props: { gridNum?: number, children: ReactNode 
 }
 
 /**
+ * @abstract
  * ARK IF條件渲染塊。只有滿足某個條件時才顯示，否則不顯示。
+ * 請勿濫用此組件！此組件僅用於包裹一些較大型的代碼，以保證代碼可讀性，而非所有條件下的最佳解決方案。
+ * 
+ * @example 一些較小的代碼塊請使用react自帶的條件渲染。
+ * {watch("type")=="WEBSITE" && (
+ *      <ContentBlock>
+ *          {`some content`}
+ *      </ContentBlock>
+ * )}
+ * 
+ * @example 一些較大型的代碼塊則可選擇使用IF組件。
+ * <IF condition={isLoaded}>
+ *      <ContentBlockGrid>
+ *          <ContentBlock>
+ *              {`some content`}
+ *          </ContentBlock>
+ *          <ContentBlock>
+ *              {`some content`}
+ *          </ContentBlock>
+ *      </ContentBlockGrid>
+ * </IF>
+ * 
  * @prop {bool} condition   渲染條件。為true時渲染子元素，為false時不渲染子元素。
  * @returns 
  */
@@ -86,12 +122,20 @@ export const IF = (props: { condition: boolean, children: ReactNode | ReactNode[
 }
 
 /**
+ * @abstract
  * ARK IFELSE條件渲染塊。條件滿足，顯示第一個子元素，否則顯示第二個子元素。
+ * 請勿濫用此組件！此組件僅用於包裹一些較大型的代碼，以保證代碼可讀性，而非所有條件下的最佳解決方案。
+ * 
+ * @example 一些較小的代碼塊請使用react自帶的條件渲染。
+ * {isEditMode ? (<div>{watch("title")}</div>) : (<input {...register("title")}/>)}
+ * 
  * @example
  * <IFELSE condition={val}>
- *      <div>...</div>   <!--   val為true時渲染   -->
- *      <div>...</div>   <!--   val為false時渲染  -->
+ *      <div>...</div>   // val為true時渲染
+ *      <div>...</div>   // val為false時渲染
+ *      <div>...</div>   // 假若這裏還有元素則會被忽略。
  * </IFELSE>
+ * 
  * @param {*} props
  * @prop {bool} val   渲染條件。為true時渲染第一個子元素，為false時渲染第二個子元素。 
  * @returns 
