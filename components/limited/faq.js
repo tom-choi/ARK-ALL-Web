8eeb7e62b565873c2e727cb876938fac5bdcab03
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-21 16:47:26 +0800

    :bee: 修補主頁代碼

--- components/limited/faq.js
+++ components/limited/faq.js
@@ -35,7 +35,7 @@ const Faq = () => {
           <div key={item.question} className="mb-5">
             <Disclosure>
               {({ open }) => (
-                <>
+                <div>
                   <Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
                     <span>{item.question}</span>
                     <ChevronUpIcon
@@ -46,7 +46,7 @@ const Faq = () => {
                   <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300">
                     {item.answer}
                   </Disclosure.Panel>
-                </>
+                </div>
               )}
             </Disclosure>
           </div>
