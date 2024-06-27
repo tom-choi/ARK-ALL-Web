99aca027389009abdde3955e9e306fe3e8fdbc06
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-27 11:13:12 +0800

    :bee: 警告banner剔除改爲hidden

--- components/micros/WarningBanner.js
+++ components/micros/WarningBanner.js
@@ -5,19 +5,19 @@ import { useTranslation } from 'react-i18next';
 const WarningBanner = () => {
     const { t } = useTranslation();
 
-    const [m_opacity, setOpacity] = useState("100");
+    const [m_opacity, setOpacity] = useState("");
 
     return (
 
         process.env.NODE_ENV == 'development' && (
             <div
-                className={`w-full flex flex-row justify-between items-center bg-alert px-3 py-2 opacity-${m_opacity} transition-all`}>
+                className={`w-full flex flex-row justify-between items-center bg-alert px-3 py-2 ${m_opacity} transition-all`}>
                 <p className="text-white">
                     <strong>{`${t("WARNING")}:`}</strong> {t("WRN_LOCAL_SERVER")}
                 </p>
                 <p>
                     <XCircleIcon className={"block text-sm text-white w-5 h-5 hover:cursor-pointer hover:scale-105 transition-all"} onClick={() => {
-                        setOpacity("0");
+                        setOpacity("hidden");
                     }} />
                 </p>
             </div>
