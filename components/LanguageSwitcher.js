460842d42d56fb58882d4d58c9d2c7c030f7a28f
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-28 16:21:28 +0800

    :bee: 優化LanguageSwitcher和DarkSwitch樣式

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -20,37 +20,19 @@ const LanguageSwitcher = () => {
     };
 
     return (
-        <div>
-            <div className="flex font-bold justify-between items-center w-24">
-                <button onClick={handleLanguageChange} value={"zh"} className="hover:text-themeColor">
-                    中
-                </button>
-                <button onClick={handleLanguageChange} value={"en"} className="hover:text-themeColor" >
-                    EN
-                </button>
-                <button onClick={handleLanguageChange} value={"ja"} className="hover:text-themeColor">
-                    日
-                </button>
-            </div>
 
-            {i18n.language === "zh" && (
-                <div className="text-sm">
-                    <h1>語言：中文</h1>
-                </div>
-            )}
-
-            {i18n.language === "en" && (
-                <div className="text-sm">
-                    <h1>Language: English</h1>
-                </div>
-            )}
-
-            {i18n.language === "ja" && (
-                <div className="text-sm">
-                    <h1>言語：日本語</h1>
-                </div>
-            )}
+        <div className="flex font-bold justify-between items-center w-24">
+            <button onClick={handleLanguageChange} value={"zh"} className={`transition-all hover:text-themeColor hover:scale-[1.02] ${curLang === "zh" ? "text-themeColor scale-[1.02]" : "text-[#000000aa]"}`}>
+                中
+            </button>
+            <button onClick={handleLanguageChange} value={"en"} className={`transition-all hover:text-themeColor hover:scale-[1.02] ${curLang === "en" ? "text-themeColor scale-[1.02]" : "text-[#000000aa]"}`}>
+                EN
+            </button>
+            <button onClick={handleLanguageChange} value={"ja"} className={`transition-all hover:text-themeColor hover:scale-[1.02] ${curLang === "ja" ? "text-themeColor scale-[1.02]" : "text-[#000000aa]"}`}>
+                日
+            </button>
         </div>
+
     );
 
 
